7d90024a9d38cc28d13943d1c224d1e6ba079035
mfaikoguz <mfaikoguz@gmail.com>
2020-04-28 14:50:32 -0500

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -3,6 +3,10 @@ package day_1;
 public class HappyBirthday {
     public static void main(String[] args) {
         // something something
+        
+        System.out.println("Happy birthday, someone!");
+        
+        // changing something from GitHub...
     }
 
 }
